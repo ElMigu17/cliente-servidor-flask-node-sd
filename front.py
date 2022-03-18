b133d1959d5c25da8e37eae82ee6a3449bf99845
ElMigu17 <rgomiguel17@gmail.com>
2022-03-17 22:51:16 -0300

    simulação de objeto e rota passando informaçoes

--- front.py
+++ front.py
@@ -1,9 +1,35 @@
 from flask import Flask
+from flask import render_template
 app =  Flask(__name__)
+
+
+lista_obj=[
+    {"id": 10,
+    "done": False,
+    "description": "Limpar quarto",
+    "deadline": "10/11"},
+    {"id": 11,
+    "done": True,
+    "description": "Tirar lixo",
+    "deadline": "10/11"},
+    {"id": 12,
+    "done": False,
+    "description": "Cozinhar torta",
+    "deadline": "15/11"},
+    {"id": 13,
+    "done": False,
+    "description": "Comprar torneira",
+    "deadline": "17/11"},
+]
 
 @app.route("/")
 def index():
     return "<h1>hi</h1>"
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('front.html', len=len(lista_obj), objects=lista_obj)
 
 if __name__ == "__main__":
     app.run()
